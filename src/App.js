93c9bdcec0f6a338038b5ab2394ed73394f0c1c5
ryanwbaker <rwbaker@ualberta.ca>
2023-11-03 17:40:32 -0600

    Finished Date counter part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+function Counter({ text, count, addFunc, subFunc }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => subFunc()}>-</button>
+      <span>
+        {text}: {count}
+      </span>
+      <button onClick={() => addFunc()}>+</button>
     </div>
   );
 }
 
-export default App;
+function DateStepCounter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  let date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <Counter
+        text="Step"
+        subFunc={() => setStep((s) => s - 1)}
+        addFunc={() => setStep((s) => s + 1)}
+        count={step}
+      />
+      <Counter
+        text="Count"
+        subFunc={() => setCount((c) => c - step)}
+        addFunc={() => setCount((c) => c + step)}
+        count={count}
+      />
+      <span>
+        {count === 0
+          ? "Today is "
+          : count > 0
+          ? `${count} day${count === 1 ? "" : "s"} from today is `
+          : `${count * -1} day${count === -1 ? "" : "s"} ago was `}
+      </span>
+      <span>{date.toDateString()}.</span>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <DateStepCounter />
+    </div>
+  );
+}
